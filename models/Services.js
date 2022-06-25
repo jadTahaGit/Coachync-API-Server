ee97548153229b97032482c5c158d729bf4704bf
Jad Taha <jadtaha.de@gmail.com>
2022-06-25 11:20:08 +0200

    Started with Services Shema  --version 01.10.92

---
+++ models/Services.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const serviceSchema = new mongoose.Schema({
+  // user_id:{}
+  Overview: {
+    title: {
+      type: String,
+      required: [true, 'Please enter a Title'],
+    },
+    username: {},
+    coachLevel: {
+      // called level in User.js
+    },
+
+    starsforservice: {
+      type: Number,
+    },
+
+    media: {
+      // should be Links
+      type: String,
+    },
+  },
+  Description: {},
+  AboutTheSeller: {},
+  FAQ: {},
+  Reviews: {},
+});
+
+const Service = mongoose.model('services', userSchema);
+
+export default User;
