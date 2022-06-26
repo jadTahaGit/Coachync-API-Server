5807c5436a45864b78e6ff2e03e003bfce6ba795
Jad Taha <jadtaha.de@gmail.com>
2022-06-26 11:25:38 +0200

    views added --version 01.11.12

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
+// view engine
+app.set('view engine', 'ejs');
+
 // database connection
 const dbURI = process.env.ATLAS_URI;
 mongoose
